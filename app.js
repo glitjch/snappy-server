6d2be968c8f368ae4d07091dcde0ee95b0a0dcf3
TJ Phan <t.jamesphan@gmail.com>
2022-05-17 20:08:01 -0700

    Delete favicon

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static("public"))
-
 require('dotenv').config()
 
 const { Configuration, OpenAIApi } = require("openai");
